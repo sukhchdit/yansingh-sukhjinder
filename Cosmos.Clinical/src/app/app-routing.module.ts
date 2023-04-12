7b34cfc50838ee527ca77f2e82e68af8283e796c
Sukhdev Singh <sukhdev.singh@imcorp.com>
2023-04-12 11:26:06 +0530

    changes in the home page

--- Cosmos.Clinical/src/app/app-routing.module.ts
+++ Cosmos.Clinical/src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./layouts/homepage-layout/homepage-layout.module').then(m => m.HomePageLayoutModule) },
-  {
-    path: '',
-    loadChildren: () =>
-      import('src/app/account/account.module').then(
-        (m) => m.AccountModule
-      ),
-  },
-  { path: '', loadChildren: () => import('./layouts/master-layout/master-layout.module').then(m => m.MasterLayoutModule) },
+  { path: '', loadChildren: () => import('./layouts/homepage-layout/homepage-layout.module').then(m => m.HomePageLayoutModule) }
+  //{
+  //  path: '',
+  //  loadChildren: () =>
+  //    import('src/app/account/account.module').then(
+  //      (m) => m.AccountModule
+  //    ),
+  //},
+  //{ path: '', loadChildren: () => import('./layouts/master-layout/master-layout.module').then(m => m.MasterLayoutModule) },
 ];
 
 @NgModule({
